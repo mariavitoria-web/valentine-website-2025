9ce7d433a62b4e10bef90997cdb1682078163862
mariavitoria-web <mariavitoria.akredito@gmail.com>
2026-06-11 19:15:17 -0300

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOW! Because i really think it's good! 🥰🧡💜",  // Shows when they go past 5000%
+        extreme: "I  think it's good, huh! 🥰🧡💜",  // Shows when they go past 5000%
         high: "To infinity and beyond!🧡💜",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
     },
