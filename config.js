aa4214572036672fc522964eb315ef14ea0068b1
mariavitoria-web <mariavitoria.akredito@gmail.com>
2026-06-11 18:50:56 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Do you like being my boyfriend? 💝",
+    pageTitle: "Do you like being my boyfriend? 🧡💜",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
